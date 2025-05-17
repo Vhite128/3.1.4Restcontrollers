6c9fe0de1d573020172f4b379a0b23d1ee6390e3
Vhite128 <vhite@internet.ru>
2025-05-18 02:08:50 +0300

    commit

--- 3.1.4 Rest controllers/src/main/java/ru/kata/spring/boot_security/demo/Exceptions/UsernameExistException.java
+++
@@ -1,9 +0,0 @@
-package ru.kata.spring.boot_security.demo.Exceptions;
-
-import org.springframework.dao.DataIntegrityViolationException;
-
-public class UsernameExistException extends DataIntegrityViolationException {
-    public UsernameExistException(String msg) {
-        super(msg);
-    }
-}
